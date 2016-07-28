4a5da9a676d346bb42b4ce371cc34c9d457cc50b
dannybk <dbukshpan@gmail.com>
2016-07-28 17:13:44 +0300

    0.2

---
+++ copy_res.sh
@@ -0,0 +1,8 @@
+ROOT_DIR="/Users/danny/AndroidDev/NewPlayer/velocee_dev_android_studio/VlcClient"
+
+#cp ~/AndroidDev/NewPlayer/velocee_dev_android_studio/VlcClient/veloceeSDK/build/outputs/aar/veloceeSDK-release.aar libs/veloceeSDK.aar 
+
+
+cp $ROOT_DIR/veloceeSDK/build/outputs/aar/veloceeSDK-release.aar libs/veloceeSDK.aar
+cp $ROOT_DIR/veloceeSDK_UI/build/outputs/aar/veloceeSDK_UI-release.aar libs/veloceeSDK_UI.aar  
+cp $ROOT_DIR/com.velocee.audioplayer/build/outputs/aar/com.velocee.audioplayer-release.aar libs/veloceeSDK_Audioplayer.aar
